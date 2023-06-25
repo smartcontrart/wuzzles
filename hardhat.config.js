cbeaafc4b6a5d33f3655e99a49409732adc411b9
Smart Contrart <smartcontrart@protonmail.com>
2023-06-25 07:51:31 -0400

    Added external network config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("hardhat-contract-sizer");
 require("@openzeppelin/hardhat-upgrades");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -9,6 +10,22 @@ module.exports = {
     optimizer: {
       enabled: true,
       runs: 200,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  networks: {
+    goerli: {
+      url: "https://goerli.infura.io/v3/" + process.env.INFURA_API_KEY,
+      accounts: [`0x` + process.env.PRIVATE_KEY],
+      timeout: 1000000,
+    },
+    mainnet: {
+      url: "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY,
+      gasPrice: 50e9,
+      accounts: [`0x` + process.env.PRIVATE_KEY],
+      timeout: 1000000,
     },
   },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,21 +7,95 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const [deployer] = await ethers.getSigners();
+  console.log(`Deployer: ${deployer.address}`);
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  const chainId = await deployer.getChainId();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  console.log(`Deploying on ${chainId}`);
 
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  const Void2122Corporation = await ethers.getContractFactory(
+    "Void2122Corporation"
   );
+  const void2122Corporation = await upgrades.deployProxy(
+    Void2122Corporation,
+    deployer
+  );
+  await void2122Corporation.deployed();
+
+  console.log(`Corporation deployed to ${void2122Corporation.address}`);
+
+  const Void2122Factory = await ethers.getContractFactory("Void2122Factory");
+  const void2122Factory = await upgrades.deployProxy(Void2122Factory, deployer);
+  await void2122Factory.deployed();
+
+  console.log(`Factory deployed to ${void2122Factory.address}`);
+
+  const Void2122Loot = await ethers.getContractFactory("Void2122Loot");
+  const void2122Loot = await upgrades.deployProxy(Void2122Loot, deployer);
+  await void2122Loot.deployed();
+
+  console.log(`Loot deployed to ${void2122Loot.address}`);
+
+  const Void2122Mod = await ethers.getContractFactory("Void2122Mod");
+  const void2122Mod = await upgrades.deployProxy(Void2122Mod, deployer);
+  await void2122Mod.deployed();
+
+  console.log(`Mod deployed to ${void2122Mod.address}`);
+
+  const Void2122Schematic = await ethers.getContractFactory(
+    "Void2122Schematic"
+  );
+  const void2122Schematic = await upgrades.deployProxy(
+    Void2122Schematic,
+    deployer
+  );
+  await void2122Schematic.deployed();
+
+  console.log(`Schematic deployed to ${void2122Schematic.address}`);
+
+  const Void2122Unit = await ethers.getContractFactory("Void2122Unit");
+  const void2122Unit = await upgrades.deployProxy(Void2122Unit, deployer);
+  await void2122Unit.deployed();
+
+  console.log(`Unit deployed to ${void2122Unit.address}`);
+
+  await void2122Factory.setLootAddress(void2122Loot.address);
+  console.log(`Loot address set in Factory`);
+  await void2122Factory.setModAddress(void2122Mod.address);
+  console.log(`Mod address set in Factory`);
+  await void2122Factory.setSchematicAddress(void2122Schematic.address);
+  console.log(`Schematic address set in Factory`);
+  await void2122Factory.setUnitAddress(void2122Unit.address);
+  console.log(`Unit address set in Factory`);
+
+  await void2122Mod.toggleAdmin(void2122Factory.address);
+  console.log(`Factory added as admin in Mod`);
+
+  await void2122Unit.toggleAdmin(void2122Factory.address);
+  console.log(`Factory added as admin in Unit`);
+
+  await void2122Unit.setModAddress(void2122Mod.address);
+  console.log(`Mod address set in Unit`);
+
+  await run("verify:verify", {
+    address: void2122Corporation.address,
+  });
+  await run("verify:verify", {
+    address: void2122Factory.address,
+  });
+  await run("verify:verify", {
+    address: void2122Loot.address,
+  });
+  await run("verify:verify", {
+    address: void2122Mod.address,
+  });
+  await run("verify:verify", {
+    address: void2122Schematic.address,
+  });
+  await run("verify:verify", {
+    address: void2122Unit.address,
+  });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
