028546ef4dbf4fa6bbed5969a5d8fe1a0f1c36af
Smart Contrart <smartcontrart@protonmail.com>
2024-04-02 17:35:02 -0400

    Deactivated deploy component

--- client/pages/deploy/index.tsx
+++ client/pages/deploy/index.tsx
@@ -9,11 +9,7 @@ let userAddress: `0x${string}` | undefined;
 
 const DeployPage: NextPage = () => {
   // const { address, connector, isConnected } = useAccount();
-  return (
-    <div className="">
-      <Deploy />
-    </div>
-  );
+  return <div className="">{/* <Deploy /> */}</div>;
 };
 
 export default DeployPage;
